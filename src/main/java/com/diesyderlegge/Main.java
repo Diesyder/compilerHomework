b4a251947e545b7e248ee0d8ec38a690a32da9e9
Diesyder <1301731816@qq.com>
2024-01-19 16:16:01 +0800

    上传初版作业

---
+++ src/main/java/com/diesyderlegge/Main.java
@@ -0,0 +1,69 @@
+package com.diesyderlegge;
+
+import java.util.Scanner;
+
+/**
+ * @author Diesyder
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("请输入要判断的数值：");
+        String input = scan.nextLine();
+        int len = input.length();
+
+        // 获得判断状态
+        String state = judgeNum(input, len);
+
+        // 输出返还的状态
+        System.out.print("判断结果：");
+        if ("Integral".equals(state)) {
+            System.out.println("输入的是整数");
+        } else if ("Fractional".equals(state)) {
+            System.out.println("输入的是小数");
+        } else {
+            System.out.println("输入的既不是整数也不是小数");
+        }
+    }
+
+    public static String judgeNum(String num, int len) {
+        String state = "start";
+        for (int i = 0; i < len; i++) {
+            switch (state) {
+                // 开始时判断符号
+                case "start" :
+                    if (num.charAt(i) == '+' || num.charAt(i) == '-') {
+                        state = "Integral";
+                    } else if (num.charAt(i) >='0' && num.charAt(i) <='9') {
+                        state = "Integral";
+                    } else {
+                        return "error";
+                    }
+                    break;
+                // 不断判断是小数点还是整数还是其他数字
+                case "Integral" :
+                    if (num.charAt(i) >='0' && num.charAt(i) <='9') {
+                        state = "Integral";
+                    } else if (num.charAt(i) == '.') {
+                        state = "Fractional";
+                    } else {
+                        return "error";
+                    }
+                    break;
+                // 判断是否为小数还是不是数值
+                case "Fractional" :
+                    if (num.charAt(i) >='0' && num.charAt(i) <='9') {
+                        state = "Fractional";
+                    } else {
+                        return "error";
+                    }
+                    break;
+                default:
+                    return "error";
+            }
+        }
+        // 返回判断状态
+        return state;
+    }
+}
